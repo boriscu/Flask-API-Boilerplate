17c6a801d72198ccbb42a46c861d7ffc42815a26
Boris <borisculjak@gmail.com>
2024-09-09 11:30:13 +0200

    Added example values

--- app/schemas/models/user_models.py
+++ app/schemas/models/user_models.py
@@ -5,13 +5,21 @@ def create_user_models(namespace):
     user_registration_model = namespace.model(
         "UserRegistration",
         {
-            "name": fields.String(required=True, description="First name of the user"),
-            "surname": fields.String(required=True, description="Surname of the user"),
+            "name": fields.String(
+                required=True, description="First name of the user", example="John"
+            ),
+            "surname": fields.String(
+                required=True, description="Surname of the user", example="Doe"
+            ),
             "email": fields.String(
-                required=True, description="Email address of the user"
+                required=True,
+                description="Email address of the user, must be unique",
+                example="john.doe@mail.com",
             ),
             "password": fields.String(
-                required=True, description="Password for account creation"
+                required=True,
+                description="Password for account creation",
+                example="Strong password",
             ),
         },
     )
@@ -19,19 +27,27 @@ def create_user_models(namespace):
     user_login_model = namespace.model(
         "UserLogin",
         {
-            "email": fields.String(required=True, description="User email address"),
-            "password": fields.String(required=True, description="User password"),
+            "email": fields.String(
+                required=True,
+                description="User email address",
+                example="john.doe@mail.com",
+            ),
+            "password": fields.String(
+                required=True, description="User password", example="Strong password"
+            ),
         },
     )
 
     user_profile_model = namespace.model(
         "UserProfile",
         {
-            "name": fields.String(description="First name of the user"),
-            "surname": fields.String(description="Surname of the user"),
-            "email": fields.String(description="Email address of the user"),
+            "name": fields.String(description="First name of the user", example="John"),
+            "surname": fields.String(description="Surname of the user", example="Doe"),
+            "email": fields.String(
+                description="Email address of the user", example="john.doe@mail.com"
+            ),
             "is_admin": fields.Boolean(
-                description="Flag noting if the user has the admin role"
+                description="Flag noting if the user has the admin role", example=False
             ),
         },
     )
