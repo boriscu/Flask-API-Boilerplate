7b0dff55312eec0f7c370c2c05d1d8acfeb0d7b6
Boris <borisculjak@gmail.com>
2025-01-13 00:26:47 +0100

    Added personal workspace to admin seeding

--- app/commands/seeding/seed_admin_command.py
+++ app/commands/seeding/seed_admin_command.py
@@ -2,9 +2,14 @@ import click
 from flask.cli import with_appcontext
 from werkzeug.security import generate_password_hash
 
+from app.init.logger_setup import LoggerSetup
+
 from config.app_config import AppConfig
 
-from app.init.logger_setup import LoggerSetup
+from app.models.enums.workspace_user_role import WorkspaceUserRole
+
+from app.models.pg.workspace import Workspace
+from app.models.pg.workspace_user import WorkspaceUser
 from app.models.pg.user_profile import UserProfile
 
 
@@ -17,7 +22,7 @@ def seed_admin_command():
     logger = LoggerSetup.get_logger("cli")
 
     if UserProfile.select().where(UserProfile.is_admin == True).count() == 0:
-        UserProfile.create(
+        new_user = UserProfile.create(
             email=AppConfig.ADMIN_EMAIL,
             password=generate_password_hash(AppConfig.ADMIN_PASSWORD),
             name="Admin",
@@ -25,6 +30,20 @@ def seed_admin_command():
             is_admin=True,
             is_active=True,
             workspace_creation_quota=None,
+        )
+
+        new_workspace = Workspace.create(
+            name="Admins' Workspace",
+            description="Workspace that belongs to the admin",
+            namespaces="[]",
+            icon_image=None,
+            is_personal=True,
+        )
+
+        WorkspaceUser.create(
+            user_id=new_user.id,
+            workspace_id=new_workspace.id,
+            workspace_user_role=WorkspaceUserRole.ADMIN.value,
         )
 
         logger.info(f"Admin account seeded successfully.")
