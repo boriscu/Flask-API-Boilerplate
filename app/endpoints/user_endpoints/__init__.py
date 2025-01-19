eb7a2ed63a6e9a4bad9be101823a25f35c73ae48
Boris <borisculjak@gmail.com>
2025-01-19 13:05:21 +0100

    Called necessary functions for user endpoints

--- app/endpoints/user_endpoints/__init__.py
+++ app/endpoints/user_endpoints/__init__.py
@@ -1,5 +1,6 @@
 from flask_restx import Namespace
 
+from app.helpers.pre_request.pre_request_manager import PreRequestManager
 from app.models.validation_schemas.retrievers.user_schema_retriever import (
     UserSchemaRetriever,
 )
@@ -10,19 +11,12 @@ user_schema_retriever = UserSchemaRetriever(user_namespace)
 
 
 def user_before_request():
-    base_path = "api/v1/user"
-    exclusions = ["/verify", "/get_myself"]
 
-    # Prepend the base path to each exclusion path
-    full_exclusions = [base_path + exc for exc in exclusions]
-
-    # Check if the path starts with the base path and is not in the exclusions list
-    if request.path.startswith(base_path) and not any(
-        request.path.startswith(exc) for exc in full_exclusions
-    ):
-        print(
-            "User-specific checks that do not apply to verify or get_myself endpoints..."
-        )
+    PreRequestManager.pre_request_handler(
+        base_path="/api/v1/user",
+        pre_request_function=UserVerificationService.abort_if_not_active,
+        exclusions=["/verify", "/get_myself"],
+    )
 
 
 from .regular_user_endpoints import *
